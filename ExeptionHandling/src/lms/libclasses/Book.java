edcaebe38f9c77ec50099fa64367d0428750dbb7
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-03-23 02:23:21 +0330

    create Library and Book classes

--- ExeptionHandling/src/lms/libclasses/Book.java
+++ ExeptionHandling/src/lms/libclasses/Book.java
@@ -1,5 +1,27 @@
 package lms.libclasses;
 
 public class Book {
+    private String title;
+    private int pageCount;
 
+    public Book (String title, int pageCount) {
+        if (title == null)
+            throw new IllegalArgumentException("Title cannot be empty");
+        if (pageCount < 1)
+            throw new IllegalArgumentException("Page count must be positive");
+        this.title = title;
+        this.pageCount = pageCount;
+    }
+
+    @Override
+    public String toString() {
+        return title + " (" + pageCount + ")";
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    public int getPageCount() {
+        return pageCount;
+    }
 }
--- ExeptionHandling/src/lms/libclasses/Library.java
+++ ExeptionHandling/src/lms/libclasses/Library.java
@@ -1,5 +1,48 @@
 package lms.libclasses;
 
-public class Library {
+import java.util.ArrayList;
 
+import lms.exeptions.EmptyLibraryException;
+import lms.exeptions.InvalidBookException;
+
+public class Library {
+    private ArrayList<Book> books = new ArrayList<Book>();
+
+    public Library() {
+    }
+
+    public void addBook(Book book) throws InvalidBookException {
+        if (book == null)
+            throw new InvalidBookException("Book should not be null");
+        books.add(book);
+    }
+    private Book findBook(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title))
+                return book;
+        }
+        return null;
+    }
+
+    public void borrowBook(String title) throws BookNotFoundException {
+            if (books.isEmpty())
+                throw new EmptyLibraryException("Library is empty, no books to borrow");
+            if (findBook(title) == null)
+                throw new BookNotFoundException("Book with title " + title + " not found");
+            System.out.println("book: " + findBook(title).toString() + " borrowed!");
+    }
+
+    public void returnBook(String title) throws BookNotFoundException {
+        if (findBook(title) == null)
+            throw new BookNotFoundException("Cannot Found. Book with title " + title + " not found");
+        System.out.println("book: " + findBook(title).toString() + " returned!");
+    }
+    public void bookList() throws EmptyLibraryException {
+        if (books.isEmpty())
+            throw new EmptyLibraryException("Library is empty");
+        System.out.println("Books in library:");
+        for (Book book : books) {
+            System.out.println(book.toString());
+        }
+    }
 }
